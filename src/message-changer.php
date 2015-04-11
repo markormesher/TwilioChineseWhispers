505073864641a45e5b9d50e245d5a90e3e95f231
AnaMJ <ana.jalba@kcl.ac.uk>
2015-04-11 17:35:43 +0100

    added dictionary and made the changer work

--- src/message-changer.php
+++ src/message-changer.php
@@ -1,5 +1,32 @@
 <?php
 
-function change($message, $source){
-    return $message;
+function changeMessage($message, $source){
+    $words = explode(' ', $message);
+    $rand = rand(0, count($words)-1);
+    $words[$rand] = getNewWord($words[$rand], $source);
+    return implode(' ',$words);
+}
+
+function getNewWord($oldWord, $source){
+    switch($source){
+        case 'urbandictionary':
+            if(strlen($oldWord) >3){
+               $oldWord = findUrbanOf(substr($oldWord,0, 3));
+            }
+            return $oldWord;
+        default:
+            return 'Y U Put the wrong source?!?!?!?';
+    }
+}
+
+function findUrbanOf($subWord){
+    $contents = file('words.txt');
+    $matches = [];
+    foreach($contents as $line){
+        if(strpos(strtolower($line), strtolower($subWord)) === 0){
+            $matches[] = $line;
+        }
+    }
+    $rand = rand(0, count($matches)-1);
+    return trim($matches[$rand]);
 }
